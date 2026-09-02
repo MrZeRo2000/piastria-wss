c6e9d33d49f4679c441858afd6f54dbb88d94a1d
MrZeRo2000 <MrZeRo2020@outlook.com>
2026-09-02 16:24:24 +0300

    Added servlet initializer, version 0.6.2

--- src/main/java/com/romanpulov/piastriawss/PiastriaWssApplication.java
+++ src/main/java/com/romanpulov/piastriawss/PiastriaWssApplication.java
@@ -2,11 +2,17 @@ package com.romanpulov.piastriawss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PiastriaWssApplication {
+public class PiastriaWssApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(PiastriaWssApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PiastriaWssApplication.class);
+	}
 }
